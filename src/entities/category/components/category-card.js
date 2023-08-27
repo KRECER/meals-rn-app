fa7428765f990ba5cc0a75f0d45fb0ccc120be5e
TretyakWeb <tretyakbro@gmail.com>
2023-08-27 03:02:00 +0300

    refactor(category-card): improve styles

--- src/entities/category/components/category-card.js
+++ src/entities/category/components/category-card.js
@@ -1,13 +1,20 @@
 import { Pressable, StyleSheet, Text, View, Platform } from 'react-native';
 
 export const Card = ({ data }) => {
-  return <View style={[styles.root, Platform.OS === 'android' && { backgroundColor: data.color }]}>
-            <Pressable android_ripple={{ color: '#ccc' }} style={({ pressed }) => pressed ? [styles.pressable, { opacity: 0.5 }] : styles.pressable}>
-                <View style={[styles.inner, Platform.OS === 'ios' && { backgroundColor: data.color }]}>
-                    <Text style={styles.title}>{data.title}</Text>
-                </View>
-            </Pressable>
-    </View>;
+  return (
+    <View style={styles.root}>
+      <Pressable
+        android_ripple={{ color: '#ccc' }}
+        style={({ pressed }) =>
+          pressed ? [styles.pressable, { opacity: 0.5 }] : styles.pressable
+        }
+      >
+        <View style={[styles.inner, { backgroundColor: data.color }]}>
+          <Text style={styles.title}>{data.title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -15,20 +22,22 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 200,
-    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
     borderRadius: 8,
-    elevation: 4
+    backgroundColor: 'white',
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    elevation: 4,
+    shadowOffset: { width: 4, height: 4 },
+    shadowRadius: 4,
+    shadowOpacity: 0.2,
+    shadowColor: '#000'
   },
   inner: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 15,
-    borderRadius: 8,
-    shadowOffset: { width: 4, height: 4 },
-    shadowRadius: 4,
-    shadowOpacity: 0.2,
-    shadowColor: '#000'
+    borderRadius: 8
+
   },
   title: {
     fontSize: 18,
