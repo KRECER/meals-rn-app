497e163d0c78a29c7c114008669ef170f1031d0e
TretyakWeb <tretyakbro@gmail.com>
2023-08-28 04:06:34 +0300

    refactor(meals-screen): delete console.log

--- src/screens/meals/meals-screen.js
+++ src/screens/meals/meals-screen.js
@@ -4,7 +4,6 @@ import { useRoute } from '@react-navigation/native';
 
 export const Screen = () => {
   const route = useRoute();
-  console.log(route.params);
   const categoryId = route.params?.categoryId;
   const { data } = mealModel.hooks.useQuery();
   const filteredMeals = data?.filter((it) => it.categoryIds.includes(categoryId));
