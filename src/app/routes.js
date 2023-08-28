80f59119036fd1ab6979403ff0929b1bb2aee70a
TretyakWeb <tretyakbro@gmail.com>
2023-08-28 11:16:55 +0300

    style: update header styles and options in Routing
    Updated the header styles and options in the Routing component in routes.js to improve the overall look and feel of the navigation headers. Applied a transparent background, customized font sizes, colors, and back button text. Also added a blur effect for the header on supported platforms.

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,14 +1,37 @@
+import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import { CategoriesScreen, MealsScreen } from '../screens';
 
 const Stack = createNativeStackNavigator();
 
 export const Routing = () => {
   return <NavigationContainer>
-    <Stack.Navigator>
-        <Stack.Screen name='Categories' component={CategoriesScreen} />
+    <Stack.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor: Platform.OS === 'ios' ? 'transparent' : '#000'
+      },
+      headerTransparent: true,
+      headerBlurEffect: 'systemChromeMaterialDark',
+      contentStyle: {
+        backgroundColor: '#F2D1A8'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontSize: 24,
+        fontWeight: '700'
+      },
+      headerBackTitle: 'Back'
+    }} >
+        <Stack.Screen name='Categories' component={CategoriesScreen} options={{
+          title: 'Categories',
+          headerLargeTitle: true,
+          headerLargeTitleStyle: {
+            fontSize: 45,
+            fontWeight: '800',
+            color: '#fff'
+          }
+        }} />
         <Stack.Screen name='Meals' component={MealsScreen} />
     </Stack.Navigator>
   </NavigationContainer>;
