af148cf9996218cbaab5deb3ce2e722472c99030
TretyakWeb <tretyakbro@gmail.com>
2023-08-28 11:16:09 +0300

    feat: add StatusBar component to App

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Routing } from './routes';
+import { StatusBar } from 'expo-status-bar';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
   return <QueryClientProvider client={queryClient}>
+    <StatusBar style='light' />
     <Routing />
   </QueryClientProvider>;
 };
