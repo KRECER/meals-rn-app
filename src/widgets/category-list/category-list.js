021738ba5ba97579647158b961ec31991fd98434
TretyakWeb <tretyakbro@gmail.com>
2023-09-01 17:47:56 +0300

    style: remove unused 'useHeaderHeight' and adjust formatting in CategoryList
    Removed the unused 'useHeaderHeight' import and adjusted the formatting in the 'CategoryList' component in 'category-list.js' for improved code clarity.

--- src/widgets/category-list/category-list.js
+++ src/widgets/category-list/category-list.js
@@ -1,16 +1,14 @@
 import { FlatList } from 'react-native';
-import { useHeaderHeight } from '@react-navigation/elements';
 import { CategoryCard, categoryModel } from '../../entities/category';
 import { useNavigation } from '@react-navigation/native';
 
 export const List = () => {
   const navigation = useNavigation();
   const { data } = categoryModel.hooks.useQuery();
-  const headerHeight = useHeaderHeight();
 
   const renderItem = (data) => {
     return <CategoryCard data={data.item} onPress={() => navigation.navigate('Meals', { categoryId: data.item.id })}/>;
   };
 
-  return <FlatList style={{ marginTop: headerHeight - 5 }} contentInsetAdjustmentBehavior="automatic" data={data} renderItem={renderItem} keyExtractor={(data) => data.id} numColumns={2}/>;
+  return <FlatList data={data} renderItem={renderItem} keyExtractor={(data) => data.id} numColumns={2}/>;
 };
