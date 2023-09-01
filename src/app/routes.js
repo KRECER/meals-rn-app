f63b47cecbdaa2b82c96c895469644bde75e875f
TretyakWeb <tretyakbro@gmail.com>
2023-09-01 17:48:23 +0300

    feat: reorganize navigation structure in Routing component
    Reorganized the navigation structure in the 'Routing' component in 'routes.js' for improved code organization. Introduced a 'DrawerNavigator' and a 'StackNavigator' to separate navigation concerns and enhance maintainability.

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,38 +1,65 @@
-import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { CategoriesScreen, MealsScreen } from '../screens';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { CategoriesScreen, MealsScreen, FavoritesScreen } from '../screens';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerGroup = () => {
+  return <Drawer.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#000'
+    },
+    headerTintColor: '#fff',
+    sceneContainerStyle: {
+      backgroundColor: '#F2D1A8'
+    },
+    drawerContentStyle: {
+      backgroundColor: '#ccad87'
+    },
+    drawerActiveBackgroundColor: '#F2D1A8',
+    drawerActiveTintColor: '#7a6143',
+    drawerInactiveTintColor: '#56432b'
+  }}>
+    <Drawer.Screen name='Drawer' component={CategoriesScreen} options={{
+      drawerIcon: ({ color, size, focused }) => <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color} />
+    }} />
+    <Drawer.Screen name='Favorites' component={FavoritesScreen} options={{
+      drawerIcon: ({ color, size, focused }) => <MaterialIcons name={focused ? 'favorite' : 'favorite-outline'} color={color} size={size} />
+    }}/>
+  </Drawer.Navigator>;
+};
+
+const StackGroup = () => {
+  return <Stack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#000'
+    },
+    contentStyle: {
+      backgroundColor: '#F2D1A8'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontSize: 24,
+      fontWeight: '700'
+    },
+    headerBackTitle: 'Back'
+  }} >
+      <Stack.Screen name='Categories' component={DrawerGroup} options={{
+        title: 'Categories',
+        headerShown: false
+      }} />
+      <Stack.Screen name='Meals' component={MealsScreen} />
+  </Stack.Navigator>;
+};
 
 export const Routing = () => {
   return <NavigationContainer>
-    <Stack.Navigator screenOptions={{
-      headerStyle: {
-        backgroundColor: Platform.OS === 'ios' ? 'transparent' : '#000'
-      },
-      headerTransparent: true,
-      headerBlurEffect: 'systemChromeMaterialDark',
-      contentStyle: {
-        backgroundColor: '#F2D1A8'
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontSize: 24,
-        fontWeight: '700'
-      },
-      headerBackTitle: 'Back'
-    }} >
-        <Stack.Screen name='Categories' component={CategoriesScreen} options={{
-          title: 'Categories',
-          headerLargeTitle: true,
-          headerLargeTitleStyle: {
-            fontSize: 45,
-            fontWeight: '800',
-            color: '#fff'
-          }
-        }} />
-        <Stack.Screen name='Meals' component={MealsScreen} />
-    </Stack.Navigator>
+    <StackGroup />
   </NavigationContainer>;
 };
