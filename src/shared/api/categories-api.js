4e6bfd37d73970c9008294f1fb6662de5d670ad7
KRECER <sudenkoweb@gmail.com>
2023-09-06 03:17:36 +0300

    feat(api): remove categories API files

--- src/shared/api/categories-api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { API_URL } from '../constants';
-
-export const categoriesApi = Object.freeze({
-  get: async () => {
-    const res = await axios.get(`${API_URL}/categories`);
-    return res.data;
-  }
-});
