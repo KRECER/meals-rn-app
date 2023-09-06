9b04032df8970c52974cdf2cb9662f38fa2f3dce
KRECER <sudenkoweb@gmail.com>
2023-09-06 03:18:19 +0300

    feat(meal-list): add navigation to meal details

--- src/widgets/meal-list/meal-list.js
+++ src/widgets/meal-list/meal-list.js
@@ -1,9 +1,16 @@
 import { FlatList } from 'react-native';
 import { MealCard } from '../../entities/meal';
+import { navigateToMealDetails } from '../../entities/meal/model/handlers';
+import { useNavigation } from '@react-navigation/native';
 
 export const List = ({ data }) => {
+  const navigation = useNavigation();
+
   const renderMealItem = (data) => {
-    return <MealCard data={data.item} />;
+    return <MealCard
+      data={data.item}
+      onPress={() => navigateToMealDetails(navigation, data.item.id)}
+    />;
   };
 
   return <FlatList data={data} keyExtractor={(data) => data.id} renderItem={renderMealItem} />;
