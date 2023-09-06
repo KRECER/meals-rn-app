798dc1a5bc2a8b1293a3ece84b14fde54cc6fc31
KRECER <sudenkoweb@gmail.com>
2023-09-06 03:18:01 +0300

    feat(navigation): add navigation constants

---
+++ src/shared/lib/navigation/constants.js
@@ -0,0 +1,38 @@
+export const screenNames = {
+  Categories: 'Categories',
+  Favorites: 'Favorites',
+  Meals: 'Meals',
+  MealDetails: 'MealDetails'
+};
+
+export const screenOptions = {
+  drawer: {
+    headerStyle: {
+      backgroundColor: '#000'
+    },
+    headerTintColor: '#fff',
+    sceneContainerStyle: {
+      backgroundColor: '#F2D1A8'
+    },
+    drawerContentStyle: {
+      backgroundColor: '#ccad87'
+    },
+    drawerActiveBackgroundColor: '#F2D1A8',
+    drawerActiveTintColor: '#7a6143',
+    drawerInactiveTintColor: '#56432b'
+  },
+  stack: {
+    headerStyle: {
+      backgroundColor: '#000'
+    },
+    contentStyle: {
+      backgroundColor: '#F2D1A8'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontSize: 24,
+      fontWeight: '700'
+    },
+    headerBackTitle: 'Back'
+  }
+};
