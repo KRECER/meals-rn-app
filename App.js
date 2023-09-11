8a055039cefe23f1cb18e83bce72b83ca1906dd8
TretyakWeb <tretyakbro@gmail.com>
2023-09-11 04:31:06 +0300

    style: update ESLint rules to disable react/display-name
    Updated the ESLint rules in the '.eslintrc.js' file to disable the 'react/display-name' rule, improving consistency in component naming practices.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { App } from './src/app/app';
+import App from './src/app/app';
 
 export default function Bootstrap () {
   return (
--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,26 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Routing } from './routes';
 import { StatusBar } from 'expo-status-bar';
+import { ModalProvider } from '../shared/lib/modal/modal.provider';
+import { ModalManager } from '../widgets/modal-manager';
+import { NavigationContainer } from '@react-navigation/native';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
-  return <QueryClientProvider client={queryClient}>
-    <StatusBar style='light' />
-    <Routing />
-  </QueryClientProvider>;
+  return <>
+      <StatusBar style='light' />
+      <Routing />
+    </>;
+};
+
+export default () => {
+  return <NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+  <ModalProvider>
+    <App />
+    <ModalManager />
+  </ModalProvider>
+</QueryClientProvider>
+  </NavigationContainer>;
 };
