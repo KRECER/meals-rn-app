4b5619e2fb1b4f567c92726d2fe4aac1217db717
TretyakWeb <tretyakbro@gmail.com>
2023-09-19 18:05:05 +0300

    feat(navigation): add stackScreenOptions and drawerScreenOptions

--- src/shared/lib/navigation/constants.js
+++ src/shared/lib/navigation/constants.js
@@ -1,3 +1,8 @@
+/**
+ * @typedef {import('@react-navigation/drawer').DrawerNavigationOptions} DrawerNavigationOptions
+ * @typedef {import('@react-navigation/native-stack').NativeStackNavigationOptions} StackNavigationOptions
+ */
+
 export const screenNames = {
   Categories: 'Categories',
   Favorites: 'Favorites',
@@ -5,34 +10,41 @@ export const screenNames = {
   MealDetails: 'MealDetails'
 };
 
-export const screenOptions = {
-  drawer: {
-    headerStyle: {
-      backgroundColor: '#000'
-    },
-    headerTintColor: '#fff',
-    sceneContainerStyle: {
-      backgroundColor: '#F2D1A8'
-    },
-    drawerContentStyle: {
-      backgroundColor: '#ccad87'
-    },
-    drawerActiveBackgroundColor: '#F2D1A8',
-    drawerActiveTintColor: '#7a6143',
-    drawerInactiveTintColor: '#56432b'
+/**
+ * Опции для экрана стека.
+ * @type {StackNavigationOptions}
+ */
+export const stackScreenOptions = {
+  headerStyle: {
+    backgroundColor: '#000'
   },
-  stack: {
-    headerStyle: {
-      backgroundColor: '#000'
-    },
-    contentStyle: {
-      backgroundColor: '#F2D1A8'
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontSize: 24,
-      fontWeight: '700'
-    },
-    headerBackTitle: 'Back'
-  }
+  contentStyle: {
+    backgroundColor: '#F2D1A8'
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontSize: 24,
+    fontWeight: '700'
+  },
+  headerBackTitle: 'Back'
+};
+
+/**
+ * Опции для экрана ящика.
+ * @type {DrawerNavigationOptions}
+ */
+export const drawerScreenOptions = {
+  headerStyle: {
+    backgroundColor: '#000'
+  },
+  headerTintColor: '#fff',
+  sceneContainerStyle: {
+    backgroundColor: '#F2D1A8'
+  },
+  drawerContentStyle: {
+    backgroundColor: '#ccad87'
+  },
+  drawerActiveBackgroundColor: '#F2D1A8',
+  drawerActiveTintColor: '#7a6143',
+  drawerInactiveTintColor: '#56432b'
 };
